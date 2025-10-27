95bf3bbf68b43ec4a2ae268e2c17bbdc53f2ac1b
Tandoori <ducloux.y@gmail.com>
2025-10-27 15:46:42 +0100

    :sparkles: [YDU]: si il contient 3 => fizz si il contient 5=> buzz and mise en place du compute

--- src/main/java/org/example/Fizzbuzz.java
+++ src/main/java/org/example/Fizzbuzz.java
@@ -8,18 +8,27 @@ public class Fizzbuzz {
     public static final int MULTIPLE_OF_FIVE = 5;
     public static final String BUZZ = "Buzz";
     public static final String FIZZ_BUZZ = FIZZ + BUZZ;
+    public static final String THREE = "3";
+    public static final String FIVE = "5";
 
-    public String calcul(int number) {
+    public void compute(int number) {
+        for (int i = 1; i <= number; i++) {
+            System.out.println(i + " => " + calculation(i));
+        }
+    }
+
+    public String calculation(int number) {
+        String numberString = String.valueOf(number);
         if(isAMultipleOf(MULTIPLE_OF_THREE, number) && isAMultipleOf(MULTIPLE_OF_FIVE, number)) {
             return FIZZ_BUZZ;
         }
-        if(isAMultipleOf(MULTIPLE_OF_THREE, number)) {
+        if(isAMultipleOf(MULTIPLE_OF_THREE, number) || numberString.contains(THREE)) {
             return FIZZ;
         }
-        if(isAMultipleOf(MULTIPLE_OF_FIVE, number)) {
+        if(isAMultipleOf(MULTIPLE_OF_FIVE, number) || numberString.contains(FIVE)) {
             return BUZZ;
         }
-        return String.valueOf(number);
+        return numberString;
     }
 
     private static boolean isAMultipleOf(int multiple, int number) {
--- src/test/java/org/example/FizzbuzzTest.java
+++ src/test/java/org/example/FizzbuzzTest.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -15,29 +14,29 @@ class FizzbuzzTest {
     void fizzbuzz_should_be_return_same_number(int number) {
         var numberExpected = String.valueOf(number);
         Fizzbuzz fizzbuzz = new Fizzbuzz();
-        var numberActual = fizzbuzz.calcul(number);
+        var numberActual = fizzbuzz.calculation(number);
         assertEquals(numberExpected, numberActual,
                 () -> "Pour le nombre " + number + ", on attendait '" + numberExpected + "', mais on a eu '" + numberActual + "'");
     }
 
     @ParameterizedTest(name = "Le nombre {0} doit retourner 'Fizz'")
-    @ValueSource(ints = {3, 6})
+    @ValueSource(ints = {3, 6, 31, 32})
     @DisplayName("Test FizzBuzz pour les nombres qui retournent 'Fizz'")
-    void fizzbuzz_should_be_return_fizz_when_is_divisible_by_three(int number) {
+    void fizzbuzz_should_be_return_fizz_when_is_divisible_or_contains_by_three(int number) {
         var numberExpected = "Fizz";
         Fizzbuzz fizzbuzz = new Fizzbuzz();
-        var numberActual = fizzbuzz.calcul(number);
+        var numberActual = fizzbuzz.calculation(number);
         assertEquals(numberExpected, numberActual,
                 () -> "Pour le nombre " + number + ", on attendait '" + numberExpected + "', mais on a eu '" + numberActual + "'");
     }
 
     @ParameterizedTest(name = "Le nombre {0} doit retourner 'Buzz'")
-    @ValueSource(ints = {5, 10})
+    @ValueSource(ints = {5, 10, 52, 56})
     @DisplayName("Test FizzBuzz pour les nombres qui retournent 'Buzz'")
-    void fizzbuzz_should_be_return_buzz_when_is_divisible_by_five(int number) {
+    void fizzbuzz_should_be_return_buzz_when_is_divisible_or_contains_by_five(int number) {
         var numberExpected = "Buzz";
         Fizzbuzz fizzbuzz = new Fizzbuzz();
-        var numberActual = fizzbuzz.calcul(number);
+        var numberActual = fizzbuzz.calculation(number);
         assertEquals(numberExpected, numberActual,
                 () -> "Pour le nombre " + number + ", on attendait '" + numberExpected + "', mais on a eu '" + numberActual + "'");
     }
@@ -48,7 +47,7 @@ class FizzbuzzTest {
     void fizzbuzz_should_be_return_fizzbuzz_when_is_divisible_by_five_and_three(int number) {
         var numberExpected = "FizzBuzz";
         Fizzbuzz fizzbuzz = new Fizzbuzz();
-        var numberActual = fizzbuzz.calcul(number);
+        var numberActual = fizzbuzz.calculation(number);
         assertEquals(numberExpected, numberActual,
                 () -> "Pour le nombre " + number + ", on attendait '" + numberExpected + "', mais on a eu '" + numberActual + "'");
     }
